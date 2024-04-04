dd1b6571c34bab364a3a33a163dbab4a8005d042
adisat <satyamahagangga@gmail.com>
2024-04-04 17:40:19 +0800

    Intro ubah

--- Introduction.py
+++ Introduction.py
@@ -1,8 +1,18 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Team 2",
-    page_icon="",
+    page_title="Web Team 2",
+    page_icon="🗿",
 )
 
 st.title("Web Team 2")
+
+st.subheader("Selamat Datang di Web Team 2 kami")
+
+st.subheader("Nama Kelompok: ")
+st.write("- I Gede Sudiartika")
+st.write("- Ni Kadek Aprianti")
+st.write("- Komang Wahyu Kusuma Wardana")
+st.write("- Anak Agung Gede Andre Kusuma")
+st.write("- Cindy Hapsari")
+st.write("- Komang Adi Satya Mahagangga")
